698f2be9f02d59fe32d65406f37e5e3f19d5670b
Robert Bagwell <erbagwell@yahoo.com>
2021-12-02 14:26:02 -0700

    add images to card

--- pages/index.js
+++ pages/index.js
@@ -9,21 +9,21 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.greeting}>
-        <h1>Hello I&lsquo;m <span className={styles.nameHighlight}>Robert Bagwell.</span></h1>
-        <h2>I&lsquo;m a web developer.</h2>
+        <h1>Hello I&rsquo;m <span className={styles.nameHighlight}>Robert Bagwell.</span></h1>
+        <h2>I&rsquo;m a web developer.</h2>
       </div>
       <div className={styles.mainImage}>
         <div className={styles.myImg} ><Image src='/me.jpg' width={250} height={250} alt="Robert Bagwell" title="Robert Bagwell" /></div>
-        <div className={styles.mainContent}>I&lsquo;m passionate about programming. I&lsquo;ve be a web developer for over {yearsExperience} years now. I have tons of hands-on
+        <div className={styles.mainContent}>I&rsquo;m passionate about programming. I&rsquo;ve be a web developer for over {yearsExperience} years now. I have tons of hands-on
           experience efficiently coding websites and applications using the latest technologies, such as NodeJS
-          and ES6. I have expert knowledge of PHP, HTML, CSS and JavaScript. I&lsquo;m constantly engaged in learning
-          and expanding my programming expertise by learning other programming languages. I&lsquo;ve done everything
-          from building site form scratch to working in a large team on just certain parts of a site. I&lsquo;ve worked
+          and ES6. I have expert knowledge of PHP, HTML, CSS and JavaScript. I&rsquo;m constantly engaged in learning
+          and expanding my programming expertise by learning other programming languages. I&rsquo;ve done everything
+          from building site form scratch to working in a large team on just certain parts of a site. I&rsquo;ve worked
           on big e-commerce site to small independently owned sites and ran the gamut of being a Full Stack
           Developer to just focusing on UX/UI.
           <br /><br />
-          So that&lsquo;s my professional background, a little about my personal life. When I&lsquo;m not programming I enjoy
-          a good movie, I&lsquo;m a huge movie buff. I also enjoy gaming to relax. Other things about me I like to
+          So that&rsquo;s my professional background, a little about my personal life. When I&rsquo;m not programming I enjoy
+          a good movie, I&rsquo;m a huge movie buff. I also enjoy gaming to relax. Other things about me I like to
           design and was a graphic designer before I started in web development.
         </div>
       </div>
@@ -32,8 +32,19 @@ export default function Home() {
         <div className={styles.demoCardContainer}>
           {MenuItems.map((cardItem, id)=>(
             <DemosCard key={id} cardItem={cardItem} />
-          ))}
-          
+          ))}  
+        </div>
+      </div>
+      <div className={styles.PLF}>
+        <h2 className={styles.pageTiltle}>Programing Languages &amp; Frameworks</h2>
+        <div className="wb">
+            <div className="description">
+                So, I was going to put this in &ldquo;Demos and fun things&rdquo; and I might in the future but I thought it is a
+                good piece to show where my strengths and weakness. This is from a real-world project I worked on, the
+                graphs update dynamically based on the data I feed them. I created this as a JavaScript plugin so I can
+                use it with any data and easily integrate it into any site.
+            </div>
+            <div id="graphDiv"></div>
         </div>
       </div>
     </div>
