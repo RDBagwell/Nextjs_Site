ddd4b6acb880b9ada5faf5bb25080d0f0901fef3
Robert Bagwell <erbagwell@yahoo.com>
2021-12-13 21:43:03 -0700

    add auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,44 +1,56 @@
 import NextAuth from "next-auth";
-import  CredentialsProvider from "next-auth/providers/credentials";
+import CredentialProvider from "next-auth/providers/credentials";
 
 export default NextAuth({
-    providers:[
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                username: { label: "Username", type: "text", placeholder: "jsmith" },
-                password: { label: "Password", type: "password" }
-              },
-              authorize:(credentials, req)=>{
-                if(credentials.username === 'john' && credentials.password === 'test' ){
-                    const user = { id: 1, name: "J Smith", email: "jsmith@example.com" };
-                    return user;
-                } else {
-                    return null;
-                }
-              }
-        })
-    ],
-    callbacks: {
-        jwt: ({token, user}) =>{
-            if(user){
-                token.id = user.id;
-            }
-            return token;
+  providers: [
+    CredentialProvider({
+      name: "credentials",
+      credentials: {
+        username: {
+          label: "Email",
+          type: "text",
+          placeholder: "johndoe@test.com",
         },
-        session: ({session, token})=>{
-            if(token){
-                session.id = token.id
-            }
-            return session
+        password: { label: "Password", type: "password" },
+      },
+      authorize: (credentials) => {
+        // database look up
+        if (
+          credentials.username === "john" &&
+          credentials.password === "test"
+        ) {
+          return {
+            id: 2,
+            name: "John",
+            email: "johndoe@test.com",
+          };
         }
+
+        // login failed
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    jwt: ({ token, user }) => {
+      // first time jwt callback is run, user object is available
+      if (user) {
+        token.id = user.id;
+      }
+
+      return token;
     },
+    session: ({ session, token }) => {
+      if (token) {
+        session.id = token.id;
+      }
+
+      return session;
+    },
+  },
+  secret: "test",
+  jwt: {
     secret: "test",
-    jwt: {
-      secret: "test",
-      encryption: true,
-    },
-    pages: {
-      signIn: "auth/sigin",
-    }
-})
+    encryption: true,
+  }
+});
